bc4442137f127acbc63880d76506f44ca2983b6c
shh <shh5946@126.com>
2020-03-30 22:25:49 +0800

    first commit

---
+++ algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithm
+//
+//  Created by 司航航 on 2020/3/30.
+//  Copyright © 2020 Si. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
